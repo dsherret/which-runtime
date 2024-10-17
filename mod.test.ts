cb47ff2253dfcf7439b0d54b99c299a27af6fd88
David Sherret <dsherret@gmail.com>
2024-10-17 19:45:27 -0400

    update

--- mod.test.ts
+++ mod.test.ts
@@ -1,7 +1,14 @@
-import * as exports from "./mod.ts";
-import { assertEquals } from "@std/assert";
+import * as values from "./mod.ts";
+
+const testsSayNode = Deno.env.get("IS_TESTING_NODE") != null;
 
 Deno.test("is deno", () => {
-  assertEquals(exports.isDeno, true);
-  assertEquals(exports.isNode, false);
+  assertEquals(values.isDeno, !testsSayNode);
+  assertEquals(values.isNode, testsSayNode);
 });
+
+function assertEquals(a: unknown, b: unknown) {
+  if (a !== b) {
+    throw new Error(`${a} !== ${b}`);
+  }
+}
